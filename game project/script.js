d654b66dcd3d5de3fac474fbda87d75bb15d4e90
Firas <96492651+firasr6@users.noreply.github.com>
2023-03-20 23:32:18 +0530

    20-3-2023

--- game project/script.js
+++ game project/script.js
@@ -1,89 +1,66 @@
 'use strict';
-// selecting elements
-const player0El = document.querySelector(`.player--0`)
-const player1El = document.querySelector(`.player--1`)
-const score0El = document.querySelector(`#score--0`)
-const score1El = document.querySelector(`#score--1`)
-const current0El = document.getElementById(`current--0`)
-const current1El = document.getElementById(`current--1`)
 
-const diceEl = document.querySelector(`.dice`)
-const btnNew = document.querySelector(`.btn--new`)
-const btnRoll = document.querySelector(`.btn--roll`)
-const btnHold = document.querySelector(`.btn--hold`)
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// starting conditions
-let scores, currentScore, activePlayer, playing
-const init = function () {
-    scores = [0, 0]
-    currentScore = 0
-    activePlayer = 0
-    playing = true
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
 
-    score0El.textContent = 0
-    score1El.textContent = 0
-    current0El.textContent = 0
-    current1El.textContent = 0
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
 
-    diceEl.classList.add(`hidden`)
 
-    player0El.classList.remove(`player--winner`)
-    player1El.classList.remove(`player--winner`)
-    player1El.classList.remove(`player--active`)
-    player0El.classList.add(`player--active`)
+const arr = [2, 3, 4]
+const a = arr[0]
+const b = arr[1]
+const c = arr[2]
 
-}
-init()
+const [x, y, z] = arr
+console.log(x, y, z);
+console.log(arr);
 
-const switchPlayer = function () {
-    document.getElementById(`current--${activePlayer}`).textContent = 0
-    currentScore = 0
-    activePlayer = activePlayer === 0 ? 1 : 0
-    player0El.classList.toggle(`player--active`)
-    player1El.classList.toggle(`player--active`)
-}
+let [main, secondary] = restaurant.categories
+console.log(main, secondary);
 
-//rolling dice functionality
-btnRoll.addEventListener(`click`, function () {
-    if (playing) {
+// switching variables
+// const temp=main
+// main = secondary
+// secondary = temp
+// console.log(main, secondary);
 
-        // 1. generating a random dice roll
-        const dice = Math.trunc(Math.random() * 6) + 1
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
 
-        //2. display dice
-        diceEl.classList.remove(`hidden`)
-        diceEl.src = `dice-${dice}.png`
+// recieve 2 return values from a 
+const [starter, mainCourse] = restaurant.order(2, 0)
+console.log(starter, mainCourse);
 
-        //3. check for rolled
-        if (dice !== 1) {
-            //add dice to current score
-            currentScore += dice
-            document.getElementById(`current--${activePlayer}`).textContent = currentScore
-        } else {
-            // switch to next player
-            switchPlayer()
-        }
-    }
-})
+const nested = [2, 4, [5, 6]]
+// const [i, , j] = nested
+const [i, , [j, k]] = nested
+console.log(i, j);
 
-btnHold.addEventListener(`click`, function () {
-    if (playing) {
-        // 1.add current score to active players score
-        scores[activePlayer] += currentScore
-        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
-
-        // 2. check if players score is >= 100
-        if (scores[activePlayer] >= 20) {
-            //finish game
-            playing = false
-            diceEl.classList.add(`hidden`)
-            document.querySelector(`.player--${activePlayer}`).classList.add(`player--winner`)
-            document.querySelector(`.player--${activePlayer}`).classList.remove(`player--actice`)
-        } else {
-            // switch to the next player
-            switchPlayer()
-        }
-    }
-})
-
-btnNew.addEventListener(`click`, init)
+const [p = 2, q = 1, r = 1] = [8, 9]
+console.log(p, q, r);
